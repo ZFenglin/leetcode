969d7701c164866e2de93690b7422cc845f97043
ZFLin <913632394@qq.com>
2021-11-25 22:45:31 +0800

    ADD 02两数相加提交

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -17,8 +17,35 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-
+var addTwoNumbers = function (l1, l2) {
+    let head = null, tail = null;
+    let flag = 0;
+    while (l1 || l2) {
+        const n1 = l1 ? l1.val : 0;
+        const n2 = l2 ? l2.val : 0;
+        const sum = n1 + n2 + flag;
+        if (!head) {
+            head = tail = new ListNode(sum % 10);
+        } else {
+            tail.next = new ListNode(sum % 10);
+            tail = tail.next;
+        }
+        flag = Math.floor(sum / 10);
+        if (l1) {
+            l1 = l1.next;
+        }
+        if (l2) {
+            l2 = l2.next;
+        }
+    }
+    if (flag > 0) {
+        tail.next = new ListNode(flag);
+    }
+    return head;
 };
 // @lc code=end
 
+
+// @after-stub-for-debug-begin
+module.exports = addTwoNumbers;
+// @after-stub-for-debug-end
