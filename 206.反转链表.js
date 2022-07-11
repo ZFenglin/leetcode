1529bcdef801f0a714866d8109cd58a1c994a1e7
ZFenglin <913632394@qq.com>
2022-07-11 18:56:44 +0800

    feat: 更新206

--- 206.反转链表.js
+++ 206.反转链表.js
@@ -16,16 +16,36 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// // 栈
+// var reverseList = function (head) {
+//     const stack = []
+//     while (head) {
+//         stack.push(head)
+//         head = head.next
+//     }
+//     if (stack.length) {
+//         let start = stack.pop()
+//         let temp = start;
+//         while (stack.length) {
+//             temp.next = stack.pop()
+//             temp = temp.next
+//         }
+//         temp.next = null
+//         return start
+//     }
+//     return null
+// }
+// 双指针
 var reverseList = function (head) {
-    let prev = null;
-    let curr = head;
-    while (curr) {
-        const next = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = next;
+    let prev = null
+    let current = head
+    while (current) {
+        const next = current.next
+        current.next = prev
+        prev = current
+        current = next
     }
-    return prev;
+    return prev
 };
 // @lc code=end
 
